0c46fecd4561e01e84859c9218183301fed6bd1a
StevennYau <stevenyau@gmail.com>
2021-03-16 15:47:12 -0400

    minor changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,33 @@
 import { USER_ACTIONS, UserContext } from "../App";
 import { useHistory } from "react-router";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const userContext = useContext(UserContext);
   let history = useHistory();
 
   // hard coded login to be successful everytime then updates user state and redirects to homepage after
   function handleClick() {
-    userContext[1]({ type: USER_ACTIONS.LOGIN });
-    history.push("/");
+    console.log("in handle click");
+    console.log(username);
+    console.log(password);
+    if (username === "username" && password === "password") {
+      userContext[1]({ type: USER_ACTIONS.LOGIN });
+      history.push("/");
+    }
+  }
+
+  // updates the current username and password variables
+  function handleInput(type, input) {
+    if (type === "username") {
+      setUsername(input);
+    }
+
+    if (type === "password") {
+      setPassword(input);
+    }
   }
 
   return (
@@ -22,6 +40,10 @@ const Login = () => {
           className="form-control"
           placeholder="Enter username"
           required
+          onChange={(input) => {
+            console.log(input.target.value);
+            handleInput("username", input.target.value);
+          }}
         />
       </div>
       <div className="form-group space-lower">
@@ -31,6 +53,10 @@ const Login = () => {
           className="form-control"
           placeholder="Enter password"
           required
+          onChange={(input) => {
+            console.log(input.target.value);
+            handleInput("password", input.target.value);
+          }}
         />
       </div>
       <button
